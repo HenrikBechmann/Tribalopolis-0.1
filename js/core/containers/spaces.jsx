5a5e5b283c3e62269fb9a2145c3dae6b56e1e10d
Henrik Bechmann <henrik@therefore.ca>
2017-03-05 16:44:11 -0500

    add some icons to the toolbar

--- js/core/containers/spaces.jsx
+++ js/core/containers/spaces.jsx
@@ -1,13 +1,50 @@
 // spaces.tsx
 import * as React from 'react';
-import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
+import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
 import { styles } from '../utilities/styles';
 // TODO: make show/hide card panel tab; make show/hide graph panel tab
+/*
+                    <IconButton>
+                        <img src='/public/icons/campfire.svg' />
+                    </IconButton>
+                    <IconButton>
+                        <FontIcon className='material-icons'>home</FontIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FontIcon className='material-icons'>settings</FontIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FontIcon className='material-icons'>refresh</FontIcon>
+                    </IconButton>
+*/
 const Spaces = (props) => (<div style={styles.frame}>
         <div style={styles.topframe}>
             <Toolbar style={styles.toolbar}>
                 <ToolbarGroup>
-                    <ToolbarTitle text="Toolbar"/>
+                    <IconButton>
+                        <FontIcon className='material-icons'>menu</FontIcon>
+                    </IconButton>
+                </ToolbarGroup>
+                <ToolbarGroup>
+                    <IconButton>
+                        <FontIcon className='material-icons'>arrow_back</FontIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FontIcon className='material-icons'>arrow_forward</FontIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FontIcon className='material-icons'>filter_list</FontIcon>
+                    </IconButton>
+                </ToolbarGroup>
+                <ToolbarGroup>
+                    <IconButton>
+                        <FontIcon className='material-icons'>account_circle</FontIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FontIcon className='material-icons'>more_vert</FontIcon>
+                    </IconButton>
                 </ToolbarGroup>
             </Toolbar>
             <div style={styles.title}>Title</div>
