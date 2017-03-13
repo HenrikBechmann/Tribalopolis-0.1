6a1871d47588d127fdcd4883918dd86f290d8e74
Henrik Bechmann <henrik@therefore.ca>
2017-03-13 13:14:36 -0400

    replace mutated styles with clones per react deprecation warning

--- js/core/components/splitter.jsx
+++ js/core/components/splitter.jsx
@@ -12,58 +12,51 @@ class Splitter extends React.Component {
             division: 50,
         };
         this.onCollapseCall = (selection) => {
-            console.log('onCollapseCall', selection);
             let collapse = this.state.collapse;
-            if (selection == 'primary') {
-                if (!collapse) {
+            let newcollapse = null;
+            if (!collapse) {
+                if (selection == 'primary') {
                     styles.topframe.bottom = '2px';
                     styles.bottomframe.top = '100%';
                     styles.splitter.bottom = '0px';
-                    this.setState({
-                        collapse: -1
-                    });
+                    newcollapse = -1;
                 }
                 else {
-                    styles.topframe.bottom = 'calc(50% + 1px)';
-                    styles.bottomframe.top = 'calc(50% + 1px)';
-                    styles.splitter.bottom = 'calc(50% - 1px)';
-                    this.setState({
-                        collapse: 0
-                    });
-                }
-            }
-            else {
-                if (!collapse) {
                     styles.topframe.bottom = '100%';
                     styles.bottomframe.top = '2px';
                     styles.splitter.bottom = 'calc(100% - 2px)';
-                    this.setState({
-                        collapse: 1
-                    });
-                }
-                else {
-                    styles.topframe.bottom = 'calc(50% + 1px)';
-                    styles.bottomframe.top = 'calc(50% + 1px)';
-                    styles.splitter.bottom = 'calc(50% - 1px)';
-                    this.setState({
-                        collapse: 0
-                    });
+                    newcollapse = 1;
                 }
             }
+            else {
+                styles.topframe.bottom = 'calc(50% + 1px)';
+                styles.bottomframe.top = 'calc(50% + 1px)';
+                styles.splitter.bottom = 'calc(50% - 1px)';
+                newcollapse = 0;
+            }
+            this.setState({
+                collapse: newcollapse
+            });
         };
     }
     render() {
         let collapse = this.state.collapse;
         styles.collapsetabtop.display = (collapse == 1) ? 'none' : 'flex';
         styles.collapsetabbottom.display = (collapse == -1) ? 'none' : 'flex';
+        // mutated values not allowed (deprectated by React); make fresh clones
+        const topframe = Object.assign({}, styles.topframe);
+        const splitter = Object.assign({}, styles.splitter);
+        const collapsetabtop = Object.assign({}, styles.collapsetabtop);
+        const collapsetabbottom = Object.assign({}, styles.collapsetabbottom);
+        const bottomframe = Object.assign({}, styles.bottomframe);
         return <div style={styles.splitterframe}>
-            <div style={styles.topframe}>
+            <div style={topframe}>
                 {this.props.primaryPane}
             </div>
-            <div style={styles.splitter}>
+            <div style={splitter}>
                 <div onClick={e => {
             this.onCollapseCall('primary');
-        }} style={styles.collapsetabtop}>
+        }} style={collapsetabtop}>
                     <FontIcon className="material-icons">
                         {!collapse ? 'arrow_drop_down' :
             (collapse == -1) ? 'arrow_drop_up' :
@@ -72,7 +65,7 @@ class Splitter extends React.Component {
                 </div>
                 <div onClick={e => {
             this.onCollapseCall('secondary');
-        }} style={styles.collapsetabbottom}>
+        }} style={collapsetabbottom}>
                     <FontIcon className="material-icons">
                         {!collapse ? 'arrow_drop_up' :
             (collapse == 1) ? 'arrow_drop_down' :
@@ -80,7 +73,7 @@ class Splitter extends React.Component {
                     </FontIcon>
                 </div>
             </div>
-            <div style={styles.bottomframe}>
+            <div style={bottomframe}>
                 {this.props.secondaryPane}
             </div>
         </div>;
--- ts/core/components/splitter.tsx
+++ ts/core/components/splitter.tsx
@@ -18,57 +18,53 @@ class Splitter extends React.Component<SplitterProps,any> {
     }
 
     onCollapseCall = (selection) => {
-        console.log('onCollapseCall',selection)
         let collapse = this.state.collapse
-        if (selection == 'primary') {
-            if (!collapse) {
+        let newcollapse = null
+        if (!collapse) {
+            if (selection == 'primary') {
                 styles.topframe.bottom = '2px'
                 styles.bottomframe.top = '100%'
                 styles.splitter.bottom = '0px'
-                this.setState({
-                    collapse:-1
-                })
+                newcollapse = -1
             } else {
-                styles.topframe.bottom = 'calc(50% + 1px)'
-                styles.bottomframe.top = 'calc(50% + 1px)'
-                styles.splitter.bottom = 'calc(50% - 1px)'
-                this.setState({
-                    collapse:0
-                })
-            }
-        } else {
-            if (!collapse) {
                 styles.topframe.bottom = '100%'
                 styles.bottomframe.top = '2px'
                 styles.splitter.bottom = 'calc(100% - 2px)'
-                this.setState({
-                    collapse:1
-                })
-            } else {
-                styles.topframe.bottom = 'calc(50% + 1px)'
-                styles.bottomframe.top = 'calc(50% + 1px)'
-                styles.splitter.bottom = 'calc(50% - 1px)'
-                this.setState({
-                    collapse:0
-                })
-            }
+                newcollapse = 1
+            } 
+        } else {
+            styles.topframe.bottom = 'calc(50% + 1px)'
+            styles.bottomframe.top = 'calc(50% + 1px)'
+            styles.splitter.bottom = 'calc(50% - 1px)'
+            newcollapse = 0
         }
+        this.setState({
+            collapse:newcollapse
+        })
     }
 
     render() {
         let collapse = this.state.collapse
         styles.collapsetabtop.display = (collapse == 1)?'none':'flex'
         styles.collapsetabbottom.display = (collapse == -1)?'none':'flex'
+        // mutated values not allowed (deprectated by React); make fresh clones
+        const topframe = Object.assign({},styles.topframe)
+        const splitter = Object.assign({},styles.splitter)
+        const collapsetabtop = Object.assign({},styles.collapsetabtop)
+        const collapsetabbottom = Object.assign({},styles.collapsetabbottom)
+        const bottomframe = Object.assign({},styles.bottomframe)
         return <div style = {styles.splitterframe}>
-            <div style={styles.topframe}>
+            <div 
+                style = {topframe}
+            >
                 {this.props.primaryPane}
             </div>
-            <div style = {styles.splitter}>
+            <div style = {splitter}>
                 <div 
                     onClick = { e => {
                         this.onCollapseCall('primary')
                     }}
-                    style={styles.collapsetabtop}
+                    style={collapsetabtop}
                 >
                     <FontIcon className="material-icons">
                         {
@@ -82,7 +78,7 @@ class Splitter extends React.Component<SplitterProps,any> {
                     onClick = { e => {
                         this.onCollapseCall('secondary')
                     }}
-                    style={styles.collapsetabbottom}
+                    style={collapsetabbottom}
                 >
                     <FontIcon className="material-icons">
                         {
@@ -93,7 +89,8 @@ class Splitter extends React.Component<SplitterProps,any> {
                     </FontIcon>
                 </div>
             </div>
-            <div style={styles.bottomframe}>
+            <div 
+                style={bottomframe}>
                 {this.props.secondaryPane}
             </div>
         </div>
