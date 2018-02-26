eccb32d81453f2ea44535093eb7d522c76fb6c2a
Henrik Bechmann <henrik@therefore.ca>
2018-02-26 06:09:46 -0500

    tweak layout

--- js/core/control/views/boxcategoriesbar.view.jsx
+++ js/core/control/views/boxcategoriesbar.view.jsx
@@ -19,7 +19,7 @@ const CategoriesBar = props => {
     return <div style={styles}>
         <FontIcon style={{ verticalAlign: 'middle', float: 'right' }} className='material-icons'>expand_less</FontIcon> 
         <FontIcon style={{ verticalAlign: 'middle' }} className='material-icons'>list</FontIcon> 
-        <span style={{ verticalAlign: 'middle' }}>Link categories</span>
+        <span style={{ verticalAlign: 'middle' }}>Activity categories</span>
     </div>;
 };
 export default CategoriesBar;
--- ts/core/control/views/quadtitlebar.view.tsx
+++ ts/core/control/views/quadtitlebar.view.tsx
@@ -3,22 +3,25 @@
 'use strict'
 
 import * as React from 'react'
+import FontIcon from 'material-ui/FontIcon'
 
 const QuadTitleBar = props => {
     return (
         <div
             style = {
                 {
+                    position:'relative',
                     height:'25px',
                     backgroundColor:'lightgray',
                     boxSizing:'border-box',
                     width:'100%',
-                    padding: '3px',
+                    padding: '3px 0 0 27px',
                     borderRadius:'5px 5px 0 0',
                     borderBottom:'1px solid silver',
                 }
             }
         >
+            <FontIcon style = {{position:'absolute',top:'0',left:'0',zIndex:3}}className='material-icons'>menu</FontIcon> 
             {props.title}
         </div>
     )
