f1377f29a98b25403810d14ec67dd3f5aff76a47
Henrik Bechmann <henrik@therefore.ca>
2018-08-31 16:39:05 -0400

    tweak

--- js/core/start/routes.jsx
+++ js/core/start/routes.jsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 // import Spaces from './control/spaces'
-import QuadspaceController from '../ui/quadspace.controller';
+import Quadspace from '../ui/quadspace.controller';
 // import ResetPassword from '../containers/resetpassword'
 // import Register from '../containers/register'
 // import RegisterPending from '../containers/registerpending'
@@ -20,7 +20,7 @@ let routedata = [
 // { path: "*", component: NoMatch }, // must be LAST, or else will pre-empt other paths
 ];
 let coreroutes = routedata.map((item, index) => (<Route key={'coreroute' + index} path={item.path} component={item.component}/>));
-let indexroute = <Route key="_WORKSPACE_" path="/workspace" component={QuadspaceController}/>;
+let indexroute = <Route key="_WORKSPACE_" path="/workspace" component={Quadspace}/>;
 let homeroute = <Route key="_HOME_" exact path="/" render={() => (<Redirect to="/workspace"/>)}/>;
 const routes = [indexroute, homeroute];
 export default routes;
--- ts/core/start/routes.tsx
+++ ts/core/start/routes.tsx
@@ -6,7 +6,7 @@ import * as React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
 // import Spaces from './control/spaces'
-import QuadspaceController from '../ui/quadspace.controller'
+import Quadspace from '../ui/quadspace.controller'
 
 // import ResetPassword from '../containers/resetpassword'
 // import Register from '../containers/register'
@@ -31,11 +31,14 @@ let coreroutes = routedata.map((item, index) => (
    <Route key = {'coreroute'+index} path={item.path} component = {item.component} />
 ))
 
-let indexroute = <Route key = "_WORKSPACE_" path = "/workspace" component={ QuadspaceController } />
+let indexroute = <Route key = "_WORKSPACE_" path = "/workspace" component={ Quadspace } />
 
-let homeroute = <Route key = "_HOME_" exact path = "/" render={() => (
-    <Redirect to="/workspace"/>
-)}/>
+let homeroute = <Route key = "_HOME_" exact 
+    path = "/" 
+    render={
+        () => (<Redirect to="/workspace"/>)
+    }
+/>
 
 const routes = [indexroute,homeroute]
 
