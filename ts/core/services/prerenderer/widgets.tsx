7efa821f29ccba7484d86a2944fbec72d6a8335f
Henrik Bechmann <henrik@therefore.ca>
2019-02-03 21:42:17 -0500

    add widgets to library

--- ts/core/services/prerenderer/widgets.tsx
+++ ts/core/services/prerenderer/widgets.tsx
@@ -9,10 +9,14 @@ import Button from '@material-ui/core/Button'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import Typeography from '@material-ui/core/Typography'
 
 const components = {
     Card,
     CardHeader,
+    CardContent,
+    Typeography,
     DialogTitle,
     Button,
 }
