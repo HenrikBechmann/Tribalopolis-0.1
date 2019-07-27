69699b32dfef283d79de449fba5357235a4ce3a9
Henrik Bechmann <henrik@therefore.ca>
2019-07-27 17:31:23 -0400

    match messages and userdata status at register page

--- src/ui/register.controller.tsx
+++ src/ui/register.controller.tsx
@@ -8,13 +8,18 @@ import React from 'react'
 import StandardToolbar from './common/standardtoolbar.view'
 import UserAccountControlData from '../services/useraccount.controldata'
 
+import administration from '../services/application'
+
 class Register extends React.Component<any,any> {
 
     render() {
+        let { userdata } = administration
         return (
             <div>
-            <StandardToolbar />
-            <p>Thanks for logging in! Now just register here and you'll be all set. [pending]</p>
+                <StandardToolbar />
+                {(userdata.status == 'loggedin') && <p>Thanks for logging in! Now just register here and you'll be all set. [pending]</p>}
+                {(userdata.status == 'signedout') && <p>Please sign in to register.</p>}
+                {((userdata.status == 'registered') || (userdata.status == 'active')) && <p>You're already regitered. Nothing to do here.</p>}
             </div>
         )
     }
