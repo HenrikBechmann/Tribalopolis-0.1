435830c841d426ed4793c30c3ff28a632858d960
Henrik Bechmann <henrik@therefore.ca>
2020-03-27 17:23:48 -0400

    rename to infinite-grid-scroll

--- src/ui/system/isbgtest/test.controller.tsx
+++ src/ui/system/isbgtest/test.controller.tsx
@@ -4,7 +4,7 @@
 import React, {useState, useRef} from 'react'
 
 // import InfiniteScroller from 'react-infinite-scroll-by-grid'// '../../common/infinitescrollbygrid/infinitescrollbygrid'
-import InfiniteScroller from '../../common/infinitescrollbygrid/infinitescrollbygrid'
+import InfiniteScroller from '../../common/infinitegridscroll/infinitegridscroll'
 import TestOptions from './testoptions'
 
 const getItem = (index) => {
