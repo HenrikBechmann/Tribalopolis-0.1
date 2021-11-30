c6d2bebb30e82a7ded5501ba7591d2a18f66496b
Henrik Bechmann <henrik@therefore.ca>
2021-11-29 20:15:18 -0500

    maintenance

--- src/data/repositories.tsx
+++ src/data/repositories.tsx
@@ -1292,7 +1292,7 @@ let lists = {
         },
         properties:{
             version:0,
-            name:'Link Types',
+            name:'Folders',
             linkedlist:false,
         },
         references:{
