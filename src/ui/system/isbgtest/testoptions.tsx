b62cd4e9f253442176b6718d32877318a385a6af
Henrik Bechmann <henrik@therefore.ca>
2022-07-29 15:24:50 -0400

    debug test

--- src/ui/system/isbgtest/testoptions.tsx
+++ src/ui/system/isbgtest/testoptions.tsx
@@ -164,7 +164,7 @@ export default function TestOptions({callbacks}) {
 
   const updateCacheValue  = (event) => {
 
-    cachevalue = parseInt(event.target.value)
+    cachevalue = event.target.value
   }
 
   const handleLayout = () => {
